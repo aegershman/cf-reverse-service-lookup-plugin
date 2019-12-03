b1534f4430f19eedb7c5f788d26cca9615c54302
Aaron Gershman <aegershman@gmail.com>
2019-12-03 15:31:04 -0600

    bump version

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func (cmd *ServiceReverseLookupCmd) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 3,
+			Build: 4,
 		},
 		Commands: []plugin.Command{
 			{
