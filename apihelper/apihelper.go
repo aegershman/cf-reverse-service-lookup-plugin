85e98b0d4558452d7c7ea1f604bb4f4824d5fc8f
Aaron Gershman <aegershman@gmail.com>
2019-12-03 13:01:39 -0600

    annnnd we'll placehold on the apihelper

---
+++ apihelper/apihelper.go
@@ -0,0 +1,26 @@
+package apihelper
+
+import (
+	"code.cloudfoundry.org/cli/plugin"
+	"github.com/aegershman/cf-report-usage-plugin/models"
+)
+
+// CFAPIHelper wraps cf curl results
+type CFAPIHelper interface {
+	GetService(string) (models.Service, error)
+}
+
+// APIHelper -
+type APIHelper struct {
+	cli plugin.CliConnection
+}
+
+// New -
+func New(cli plugin.CliConnection) CFAPIHelper {
+	return &APIHelper{cli}
+}
+
+// GetService -
+func (api *APIHelper) GetService(serviceGUID string) (models.Service, error) {
+	return models.Service{}, nil
+}
