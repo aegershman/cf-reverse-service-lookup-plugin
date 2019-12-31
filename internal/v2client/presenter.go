c92becb923bad2adc270ca7b2fe40520a23b7b47
Aaron Gershman <aegershman@gmail.com>
2019-12-31 16:07:39 -0600

    presentation refactor (#15)

--- internal/v2client/presenter.go
+++ internal/v2client/presenter.go
@@ -1,29 +1,73 @@
 package v2client
 
 import (
+	"encoding/json"
+	"io"
+
 	log "github.com/sirupsen/logrus"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 // Presenter -
 type Presenter struct {
-	ServiceReport []ServiceReport
-	Format        []string
+	formats        []string
+	serviceReports []ServiceReport
+	writer         io.Writer
+}
+
+// NewPresenter -
+func NewPresenter(formats []string, serviceReports []ServiceReport, writer io.Writer) *Presenter {
+	return &Presenter{
+		formats:        formats,
+		serviceReports: serviceReports,
+		writer:         writer,
+	}
 }
 
 // Render -
 func (p *Presenter) Render() {
-	if len(p.Format) == 0 {
+	if len(p.formats) == 0 {
 		p.asTable()
 	}
 
-	for _, f := range p.Format {
+	for _, f := range p.formats {
 		switch f {
 		case "table":
 			p.asTable()
 		case "json":
 			p.asJSON()
 		default:
-			log.Debugf("unknown format [%s], using default\n", p.Format)
+			log.Debugf("unknown format [%s], using default\n", f)
 		}
 	}
+}
+
+func (p *Presenter) asTable() {
+	table := tablewriter.NewWriter(p.writer)
+	table.SetHeader([]string{
+		"service_guid",
+		"service",
+		"org",
+		"space",
+	})
+
+	for _, report := range p.serviceReports {
+		table.Append([]string{
+			report.Service.GUID,
+			report.Service.Name,
+			report.Organization.Name,
+			report.Space.Name,
+		})
+	}
+
+	table.Render()
+}
+
+func (p *Presenter) asJSON() {
+	j, err := json.Marshal(p.serviceReports)
+	if err != nil {
+		log.Debugln(err)
+	}
+	p.writer.Write(j)
 }
